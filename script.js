c1d38213134d89130920f8111700b969b8fa7e7f
Cameron <camerongaskill@gmail.com>
2021-06-03 15:15:52 -0400

    Created UI, stuck on js function

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $("#form").submit(function(e){
+        e.preventDefault()
+        //gets number from user input
+        let num = document.getElementById("num").value
+
+    })
+})
